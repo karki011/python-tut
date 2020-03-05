0099f80b001786b4f408c4e6eb1d7acad4369ac1
Binup karki <karki011@gmail.com>
2020-03-05 12:54:13 -0600

    Inherentance

---
+++ 4-classes.py
@@ -0,0 +1,52 @@
+# class is blueprint for creating new objects
+# object is an instances of a class
+
+
+# class Point:
+#     default_color = "red"
+
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
+
+#     def draw(self):
+#         print(f"Point ({self.x}, {self.y})")
+
+
+# point = Point(1, 2)
+# point.draw()
+
+
+# Inhertiance
+
+
+class InvalidOperationError(Exception):
+    pass
+
+
+class Stream:
+    def __init__(self):
+        self.opened = False
+
+    def open(self):
+        if self.opened:
+            raise InvalidOperationError("Stream already open")
+        self.opened = True
+
+    def close(self):
+        if not self.close:
+            raise InvalidOperationError("Stream already closed")
+        self.opened = False
+
+
+class FileStream(Stream):
+    def read(self):
+        print("Reading data from a file")
+
+
+class NetworkStream(Stream):
+    def read(self):
+        print("Reading data from a network")
+
+
+
